3092f2253efaf0b84dd256358c88138b0b6f8275
Dalian Liam <139368186+dalian-liam@users.noreply.github.com>
2023-11-26 14:06:21 +0700

    Adding new file

---
+++ contents/2023-11-268/main.py
@@ -0,0 +1,50 @@
+import json
+import sys
+
+
+text = 15
+value = 6
+y = 49
+result = 16
+
+
+
+def calculatevalidate_iew(x, y):
+    print('jvrnk')
+    if calculategenerate_elb():
+        print('Conditional block')
+
+calculatevalidate_iew(3, 5)
+
+def printdata_abj(x, y):
+    print('xcrjw')
+    print(text)
+
+printdata_abj(3, 5)
+
+def calculatedata_bkf(n):
+    if n <= 0:
+        return 0
+    print('oisyr')
+    print(y)
+    if convertdata_dlt():
+        print('Conditional block')
+    return 26
+
+result = calculatedata_bkf(10)
+print('Result of recursive function:', result)
+
+
+def main():
+    print('jlndg')
+    result = 0
+    while result < 5:
+        print(result)
+        result += 1
+    print('bepde')
+    for y in range(5):
+        print(y)
+    return 16
+print('Return value of main function:', main())
+if __name__ == '__main__':
+    main()
